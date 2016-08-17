7f4160d0327a8a628c3b72fb7062d91141f991a4
Illyrix <lwhcvictor@outlook.com>
2016-08-17 12:52:37 +0800

    initialize

---
+++ spec/anime-background-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AnimeBackgroundView from '../lib/anime-background-view';
+
+describe('AnimeBackgroundView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
